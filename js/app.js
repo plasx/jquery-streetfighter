39a502d070176b69b14520fbbe76913fd98848c6
plasx <plasx@live.com>
2015-10-31 21:45:39 -0400

    made changes showing comments for things to do in my events

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
 $(document).ready(function(){
-	$('.ryu').mouseenter(function(){
+	$('.ryu').mouseenter(function(){ // event "Mouse enter event"
+		//alert('mousu entered .ryu div'); // method call "alert method call"
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
-
-			 .mouseleave(function(){ //chained from the first one $('.ryu').
+	})
+	.mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
-
-			 .mousedown(function(){
-		console.log('mousedown');
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//play hadouken sfx
-		//shw hadouken n animate
 	})
-			 .mouseup(function(){
+	.mousedown(function(){ // event callback functions
+		//play hadouken sound
+		//show hadouken and animate it flyingaway
+	})
+	.mouseup(function(){
 		console.log('mouseup');
-		// ryu goes back to his ready position
-		$('.ryu-thorwing').hide();
-		$('.ryu-ready').show();
+		//ryu goes back to his ready state.
 	});
 });
